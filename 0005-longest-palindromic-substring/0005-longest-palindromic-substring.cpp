b4dea7682ead6ec6b6ff01560a6a35d175cd5ea5
Deepikasharma-1605 <deepikaper09@gmail.com>
2026-07-22 12:46:37 +0530

    Time: 785 ms (10.3%), Space: 13.8 MB (42.07%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(string &s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.length();
+        string lsize = "";
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                if (isPalindrome(s, i, j)) {
+
+                    if (j - i + 1 > lsize.size()) {
+                        lsize = s.substr(i, j - i + 1);
+                    }
+
+                }
+            }
+        }
+
+        return lsize;
+    }
+};

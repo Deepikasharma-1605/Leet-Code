cd2bb5043245b14849cd4981eb954111da812a66
Deepikasharma-1605 <deepikaper09@gmail.com>
2026-08-30 11:50:41 +0530

    Time: 0 ms (100%), Space: 9 MB (7.94%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,7 @@
 class Solution {
 public:
-
-    double pow(double x , long long n){
-        long long answer = 1;
-        if(n == 0){
-            return 1;
-        }
-        double ans = pow(x , n/2);
-        if(n % 2 == 0){
-            return ans * ans;
-        }
-        return ans * ans * x;
-        
-    }
     double myPow(double x, int n) {
-        long long N = n;
-        if(n < 0){
-            x = 1 / x;
-           N = -N;
-        }
         return pow(x , n);
+        
     }
 };

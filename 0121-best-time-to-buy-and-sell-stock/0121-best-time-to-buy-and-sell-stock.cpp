b43160760addfdd67ab01b9457cf943fe4467fa6
Deepikasharma-1605 <deepikaper09@gmail.com>
2026-09-09 15:50:03 +0530

    Time: 2 ms (30.65%), Space: 97.2 MB (88.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int maxp = 0;
         for(int i = 0 ; i < prices.size() ; i++){
             minp = min(minp , prices[i]);
-            maxp =  max(maxp , prices[i] - minp);
+            maxp = max(maxp , prices[i] - minp);
         }
         return maxp;
     }

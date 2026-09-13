d4f2d5e7e66e3b2b960105195138981cd8be9499
Deepikasharma-1605 <deepikaper09@gmail.com>
2026-09-13 18:52:27 +0530

    Time: 0 ms (100%), Space: 21.1 MB (17.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -7,7 +7,6 @@ public:
         vector<int> cols(n, 0);
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-
                 if(matrix[i][j] == 0) {
                     rows[i] = 1;
                     cols[j] = 1;
@@ -16,7 +15,6 @@ public:
         }
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-
                 if(rows[i] == 1 || cols[j] == 1) {
                     matrix[i][j] = 0;
                 }

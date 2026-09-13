ec691531a890557a8a16bc068e850aab0b040f36
Deepikasharma-1605 <deepikaper09@gmail.com>
2026-09-13 14:41:55 +0530

    Time: 7 ms (46.44%), Space: 20 MB (34.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    bool dfs(int node ,int numCourses, vector<vector<int>>& adj , vector<bool> &visited , vector<bool> &result){
-        
+    bool dfs(int node , vector<vector<int>> &adj , vector<bool> &visited , vector<bool> &result){
         visited[node] = true;
         result[node] = true;
         for(int neighbour : adj[node]){
             if(!visited[neighbour]){
-                if(dfs(neighbour , numCourses , adj , visited , result)){
+                if(dfs(neighbour , adj , visited , result)){
                     return true;
                 }
             }else if(result[neighbour]){
                 return true;
             }
-
         }
         result[node] = false;
         return false;
-
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto p : prerequisites){
-            int course = p[0];
-            int pre = p[1];
-            adj[pre].push_back(course);
-        }
         vector<bool> visited(numCourses , false);
         vector<bool> result(numCourses , false);
+        vector<vector<int>> adj(numCourses);
+        for(auto p : prerequisites){
+            int x = p[0];
+            int y = p[1];
+            adj[y].push_back(x);
+        }
+        bool found = true;
         for(int i = 0 ; i < numCourses ; i++){
             if(!visited[i]){
-                if(dfs(i , numCourses , adj , visited , result)){
-                    return false;
+                if(dfs(i , adj , visited , result)){
+                    found = false;
+                    break;
                 }
             }
         }
-        return true;
+        return found;
     }
 };

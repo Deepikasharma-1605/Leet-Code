3e29ba0c074dd46bfb0545781ad1421a616251e2
Deepikasharma-1605 <deepikaper09@gmail.com>
2026-09-16 11:17:50 +0530

    Time: 0 ms (100%), Space: 9.3 MB (7.98%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
     int myAtoi(string s) {
-
-        long long num = 0;
-        string a = "";
         int sign = 1;
-
+        string ans = "";
         int i = 0;
-
-        // Skip leading spaces
-        while(i < s.length() && s[i] == ' ')
-            i++;
-
-        // Check sign
-        if(i < s.length() && (s[i] == '+' || s[i] == '-')){
-            if(s[i] == '-')
-                sign = -1;
+        while(i < s.length() && s[i] == ' '){
             i++;
         }
-
-        // Your same logic
-        for(; i < s.length(); i++){
-            if(isdigit(s[i])){
-                a += s[i];
+        if(i < s.length() && (s[i] == '-' || s[i] == '+')){
+            if(s[i] == '-'){
+                sign = -1;
             }
-            else{
+            i++;
+        }
+        for(; i < s.length() ; i++){
+            if(isdigit(s[i])){
+                ans+=s[i];
+            }else{
                 break;
             }
         }
-        for(int i = 0 ; i < a.length() ; i++){
-            num = num * 10 + (a[i] - '0');
-
-            if(sign == 1 && num > INT_MAX)
+        long long answer = 0;
+        cout << ans << endl;
+        for(int i = 0 ; i < ans.length() ; i++){
+            answer = answer * 10 + (ans[i] - '0');
+            if(sign == 1 && answer > INT_MAX){
                 return INT_MAX;
-
-            if(sign == -1 && -num < INT_MIN)
+            }
+            if(sign == -1 && -answer < INT_MIN){
                 return INT_MIN;
+            }
         }
-
-        return sign * num;
+        return sign * answer;
     }
 };
